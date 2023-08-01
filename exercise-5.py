6609d4d723a841a2cd33c2d90a005b3d7e1086aa
longm12 <137611717+longm12@users.noreply.github.com>
2023-08-01 16:37:14 +0700

    by Long

--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,14 @@
 def reverse_ascending(numbers):
-    ...
+    result = []
+    start = 0
+    end = 0
+
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i + 1] and start != end:
+            result.extend(numbers[end:start - 1:-1])
+            start = end + 1
+            end = i + 1
+        if start != end:
+            result.extend(numbers[end:start - 1:-1])
+    return result
+
